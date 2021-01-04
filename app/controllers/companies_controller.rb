6fe4d0069cf83c4bc260a3c758cffe24c6c0827a
Daniel <danielnottingham.ti@gmail.com>
2021-01-04 15:39:32 -0300

    Added form for company and active storage

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,32 @@
 class CompaniesController < ApplicationController
   def new
+    if user_signed_in?
+      @company = current_user.build_company
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def edit
   end
 
   def create
+    @company = current_user.build_company
+    if @company.save
+      flash[:notice] = 'Empresa cadastrada com sucesso!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Erro ao cadastrar a empresa.'
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
   end
 end
