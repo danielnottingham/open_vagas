a4462c0ad6a55673fc2220276b57e77e34929499
Daniel <danielnottingham.ti@gmail.com>
2021-01-07 10:30:40 -0300

    Create button publish position and added CRUD of position

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,9 @@
 class Position < ApplicationRecord
   belongs_to :company
+
+  enum career: %i[developer business_intelligence information_technology design
+                  product technology other]
+  enum contract: %i[clt pj match]
+
+  validates :name, :career, :contract, :city, :state, :summary, :description, presence: true
 end
